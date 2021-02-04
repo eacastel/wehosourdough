dc90ffe9bf2a225681891874c403d2dfd7f6ba58
eacastel <emilio@aliigo.com>
2021-02-04 12:12:27 -0800

    price change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
             <div className="mt-4 pt-4 text-center max-w-xl mx-auto">
                 <h1>Place an Order for Pickup!</h1>
                 <p>Fill out the form below to place an order. <br />Or call now:  <a href="tel:+1-‪323-963-3322‬"><span className="font-bold hover:underline">(323) 963-3322‬.</span></a></p>
-                <p className="border border-brown shadow-md mt-4 p-4 rounded-md">All sourdough bread is made to order and requires payment in advance. Once we receive the order will will contact you to arrange payment via Venmo or Paypal. <br /><span className="font-bold">Price per loaf is $14. Curbside pick-up!</span></p>
+                <p className="border border-brown shadow-md mt-4 p-4 rounded-md">All sourdough bread is made to order and requires payment in advance. Once we receive the order will will contact you to arrange payment via Venmo or Paypal. <br /><span className="font-bold">Price per loaf is $12. Curbside pick-up!</span></p>
                 
             </div>
             <ContactForm className="" />
