a0de187631965b28aaddc735e772b5f73be1b215
eacastel <emilio@aliigo.com>
2021-02-06 15:15:43 -0800

    footer order button

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 
 
 const Footer = () => {
 
-
   return (
-    <footer className="">
-        <div className="mt-8 bg-brown p-4 text-sm text-white text-center">
-            <p>{new Date().getFullYear()} &copy; All Rights Reserved Weho Sourdough</p> 
-
+    <footer className="sticky bottom-0 z-50 w-full bg-brown">
+      <div className="flex flex-row justify-between h-12">
+        <div className="flex-auto self-center text-sm text-bggray ml-8">
+          {new Date().getFullYear()} &copy; All Rights Reserved
         </div>
+        <div className="flex-auto self-center text-right mr-8">
+          <Link to="contact">
+              <button id="button" type="button" className="shadow-md cursor-pointer bg-wheat hover:text-wheat hover:shadow-none hover:bg-text rounded text-text font-medium px-2 pt-1 pb-2 mt-1 text-sm"> Order Here 
+              </button>
+          </Link>
+        </div>
+      </div>
+      
     </footer>
   )
 };
