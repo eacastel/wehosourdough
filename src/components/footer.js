e44b7307294491b4ca4765cb4ea69d6539856af4
eacastel <emilio@aliigo.com>
2021-02-06 16:37:09 -0800

    same-width feature and footer margin

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby'
 const Footer = () => {
 
   return (
-    <footer className="sticky bottom-0 z-50 w-full bg-brown">
+    <footer className="sticky bottom-0 z-50 w-full bg-brown mt-8">
       <div className="flex flex-row justify-between h-12">
         <div className="flex-auto self-center text-sm text-bggray ml-8">
           {new Date().getFullYear()} &copy; All Rights Reserved
