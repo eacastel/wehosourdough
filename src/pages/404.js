e09995e99da137583cd19288f5797e1b45f09345
eacastel <emilio@aliigo.com>
2021-02-16 15:13:02 -0800

    initial-test

--- src/pages/404.js
+++ src/pages/404.js
@@ -25,7 +25,7 @@ const NotFoundPage = () => {
     <Layout>
       <SEO title="404 Not Found" />
       <div className="m-4">
-        <h1 className="font-normal text-3xl text-center mt-8">Crumbs! We did not find the page you wanted.</h1>
+        <h1 className="font-normal text-3xl text-center mt-8">Crumbs! We did not find the page you wanted...</h1>
         <p className="mb-8 mt-0 text-center hover:underline"><Link to="/">Click this link to go to the home page</Link>.</p>
         <Img fluid={data.crumbsImg.childImageSharp.fluid} alt="Image of bread crumbs on cutting board" />
       </div>
