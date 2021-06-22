8f921af454dd4c7946e08ee40279aa624c34527f
eacastel <emilio@aliigo.com>
2021-06-21 21:39:01 -0700

    holiday message noclick 2

--- src/components/closedForSummer.js
+++ src/components/closedForSummer.js
@@ -3,15 +3,10 @@ import React from 'react'
 const ButtonClosed = () => {
   return (
     <div className='p-6 md:pl-0 text-center md:text-left'>
-      <button
-        id='button'
-        type='button'
-        className='shadow-md border border-brown bg-darkred text-white font-medium py-3 px-8 lg:py-4 lg:px-12 xl:py-5 xl:px-16 xl:text-lg'
-      >
-        {' '}
+      <div className='shadow-md border text-center border-brown bg-darkred text-white font-medium py-3 px-8 lg:py-4 lg:px-12 xl:py-5 xl:px-16 xl:text-lg'>
         WE WILL BE CLOSED FOR THE SUMMER UNTIL AUGUST 20TH. THANK YOU FOR YOUR
         PATRONAGE!
-      </button>
+      </div>
     </div>
   )
 }
