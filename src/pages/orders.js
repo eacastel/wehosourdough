11e16b5f3e9be97f28c535ffff31d04e73f671dd
eacastel <emilio@aliigo.com>
2022-09-27 00:46:52 -0700

    for sale3

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -11,28 +11,18 @@ const Contact = () => {
         <SEO title='Order Form' />
         <main>
           <div className='mt-4 pt-4 text-center max-w-xl mx-auto'>
-            <h1>Place an Order for Pickup!</h1>
-            <p>
-              To place an order fill out the form below send us a text or call
-              now: <br />
-              <a href='tel:+1-‪323-963-3322‬'>
-                <span className='font-bold hover:underline'>
-                  (323) 963-3322‬.
-                </span>
-              </a>
-            </p>
+            <h1>We are no longer taking orders!</h1>
+      
             <p className='border border-brown shadow-md mt-4 p-4 rounded-md'>
-              All sourdough bread is made to order and requires payment in
-              advance. Once we receive the order we will contact you to arrange
-              payment via Venmo or Paypal. <br />
+              We've moved and no longer taking orders. Thank you for your interest! <br />
               <span className='font-bold'>
-                Price per loaf is $12. Curbside pick-up!
+                
               </span>
             </p>
           </div>
           <ContactForm className='' />
         </main>
-        <FooterOrder />
+        
       </Layout>
     </div>
   )
