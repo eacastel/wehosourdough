953e639485612d72249eef2da5a0af9d31f0da09
eacastel <emilio@aliigo.com>
2024-01-18 13:22:16 -0800

    last images plugin

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const Header = () => {
   const data = useStaticQuery(graphql`
     query {
       wehoLogo: file(relativePath: { eq: "weho-sourdough-color-1800.png" }) {
         childImageSharp {
-          fluid(maxWidth: 1600) {
-            ...GatsbyImageSharpFluid
-          }
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+            quality: 100
+            maxWidth: 1600
+          )
         }
       }
     }
   `);
 
+  const image = getImage(data.wehoLogo);
+
   return (
     <header className="sticky top-0 bg-white shadow z-50">
         <div className="w-full">
             <Link to="/">
-                <Img fluid={data.wehoLogo.childImageSharp.fluid} alt="Weho Sourdough Logo" className="lg:max-w-4xl mx-auto" />
+                <GatsbyImage image={image} alt="Weho Sourdough Logo" className="lg:max-w-4xl mx-auto" />
             </Link>
         </div>
     </header>
